988d5b9c2c9223d2c3a5be3018ff7b124e8e571f
Rafael Abreu <rafaelabreu@outlook.com>
2015-10-09 22:11:08 -0300

    Add MIT License

--- config/routes.php
+++ config/routes.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Copyright (c) Rafael Abreu
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://www.rafaelabreu.eti.br CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 use Cake\Routing\Router;
 
 Router::plugin('Acl', function ($routes) {
